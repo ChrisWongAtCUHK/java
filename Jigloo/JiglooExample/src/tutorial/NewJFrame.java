218359011657cdc3363a0e34e7eab75e9c9961cc
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-07-30 17:23:38 +0800

    Jigloo examples for non-commercial ust

---
+++ Jigloo/JiglooExample/src/tutorial/NewJFrame.java
@@ -0,0 +1,75 @@
+package tutorial;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class NewJFrame extends javax.swing.JFrame {
+	private JButton jButton1;
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				NewJFrame inst = new NewJFrame();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public NewJFrame() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			GridBagLayout thisLayout = new GridBagLayout();
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			thisLayout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
+			thisLayout.rowHeights = new int[] {7, 7, 7, 7};
+			thisLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1};
+			thisLayout.columnWidths = new int[] {7, 7, 7, 7};
+			getContentPane().setLayout(thisLayout);
+			{
+				jButton1 = new JButton();
+				getContentPane().add(jButton1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+				jButton1.setText("Click me");
+				jButton1.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						System.out.println("Chris Wong is handsome");
+						JOptionPane.showMessageDialog(null, "Hello World");
+					}
+				});
+			}
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Jigloo/README.txt
@@ -0,0 +1,11 @@
+Reference:
+	http://www.cloudgarden.com/jigloo/
+	http://www.eclipse.org/swt/
+	
+Notes: 
+	1.	jigloo_464.zip 
+			~Unzip it into the eclipse folder so that the structure is eclipse/plugins/com.cloudgarden.jigloo_4.#.#
+	2.	swt-4.3-win32-win32-x86.zip 
+			~Unzip it and get swt.jar
+
+	
