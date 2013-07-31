88dd9d875f5f7d65f5a37a98e2255e09270700ac
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-07-31 10:28:46 +0800

    Tutorial from http://duoenote.blogspot.hk/2013/05/eclipe-windowbuolder-java-hello-world.html

---
+++ WindowBuilder/WindowBuilderExample/src/ChrisFrame.java
@@ -0,0 +1,92 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class ChrisFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextArea textArea;
+	private JButton btnChrisWong;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChrisFrame frame = new ChrisFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ChrisFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridwidth = 2;
+		gbc_textArea.insets = new Insets(0, 0, 5, 5);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 0;
+		contentPane.add(textArea, gbc_textArea);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 0;
+		gbc_textField.gridy = 1;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		btnChrisWong = new JButton("Chris Wong");
+		btnChrisWong.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton source = (JButton)e.getSource();
+				getTextArea().append(source.getText() + "is handsome\n");
+			}
+		});
+		GridBagConstraints gbc_btnChrisWong = new GridBagConstraints();
+		gbc_btnChrisWong.gridx = 1;
+		gbc_btnChrisWong.gridy = 1;
+		contentPane.add(btnChrisWong, gbc_btnChrisWong);
+	}
+	
+	private JTextArea getTextArea(){
+		return textArea;
+	}
+
+}
