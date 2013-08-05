12ae6008610f7ff74085e7277c0a9fbfd8157a35
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-08-05 15:30:40 +0800

    button & label css

--- JavaFX/GettingStartedWithJavaFX/src/form/Login.java
+++ JavaFX/GettingStartedWithJavaFX/src/form/Login.java
@@ -35,7 +35,8 @@ public class Login extends Application {
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        //scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        scenetitle.setId("welcome-text");
         grid.add(scenetitle, 0, 0, 2, 1);
 
         Label userName = new Label("User Name:");
@@ -60,8 +61,10 @@ public class Login extends Application {
         	 
             @Override
             public void handle(ActionEvent e) {
-                actiontarget.setFill(Color.FIREBRICK);
+                //actiontarget.setFill(Color.FIREBRICK);
+            	actiontarget.setId("actiontarget");
                 actiontarget.setText("Sign in button pressed");
+                
             }
         });
         
